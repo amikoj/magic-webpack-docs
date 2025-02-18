fe551d8aeb8d8df5a614e3c9fad8748593cf889a
hfcai <haifei.cai@gientech.com>
2025-02-18 11:24:00 +0800

    feat: 首次提交创建模板

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Webpack黑魔法研习社</span>,
+  project: {
+    link: 'https://github.com/amikoj/mini-magic-webpack.git',
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
